14148e805e35873db9d81546d3278a8810f5d4ce
Jamieson Leibovitch <jamieson.leibovitch@uoit.net>
2020-03-26 21:29:13 -0400

    updated skeleton code for functionality

--- banker.c
+++ banker.c
@@ -18,54 +18,88 @@ int allocation[NUM_CUSTOMERS][NUM_RESOURCES];
 // Remaining need of each customer (max-allocation)
 int need[NUM_CUSTOMERS][NUM_RESOURCES];
 
+pthread_mutex_t mux;
+
 // Customer functions
 bool request_res(int n_customer, int request[]);
 bool release_res(int n_customer, int release[]);
-
 void fill_matrix();
 
 
-int main(int argc, char *argv[]){
 
-    // pthread_t threads[NUM_CUSTOMERS];
-    // int rc;
-
-    for (int i = 1; i < argc; i++){
-        available[i] =  atoi(argv[i]);
+void print_table() {
+    printf("Allocation       Max       Need\n");
+    for (int i = 0; i < NUM_CUSTOMERS; i++) {
+            printf("%d %d %d            %d %d %d     %d %d %d\n", 
+            allocation[i][0],allocation[i][1],allocation[i][2],
+            max[i][0],max[i][1],max[1][2],
+            need[i][0],need[i][1],need[i][2]);
     }
-    
-    fill_matrix();
-
-    // while (true){
-    //     for (int i = 0; i < NUM_CUSTOMERS; i++){
-    //         rc = pthread_create(&threads[i], request_res, i, allocation);
-    //         if (rc){
-    //             printf("ERROR; return code from pthread_create() is %d\n", rc);
-    //             exit(-1);
-    //         }
-    //     }
-    //     break;
-    // }
 }
 
 void fill_matrix(){
 
     for (int i = 0; i < NUM_CUSTOMERS; i++){
         for (int j = 0; j < NUM_RESOURCES; j++){
-            allocation[i][j] = (rand() % 4);
-            max[i][j] = (rand() % 7) + 3;
+            allocation[i][j] = 0;
+            max[i][j] = (rand() % available[j]);
             need[i][j] = max[i][j] - allocation[i][j];
         }
     }
-
-    printf("Allocation     Max     Need\n");
-    for (int i = 0; i < NUM_CUSTOMERS; i++){
-            printf("%d%d%d            %d%d%d     %d%d%d\n", allocation[i][0],allocation[i][1],allocation[i][2],max[i][0],max[i][1],max[1][2],need[i][0],need[i][1],need[i][2]);
-    }
+   
 }
 
+int sum_need(int n_customer) {
+    int sum = 0;
+    pthread_mutex_lock(&mux);
+    for (int i = 0; i < NUM_RESOURCES; i++) {
+        sum+= need[n_customer][i];
+    }
+    pthread_mutex_unlock(&mux);
+
+    return sum;
+
+}
+
+void* customer(void* customer_id_ptr) {
+
+    int customer_id = *(int*)(customer_id_ptr);
+
+    printf("Customer Id: %d\n", customer_id);
+
+    while(true) {
+
+        int request[NUM_RESOURCES];
+        int sum_request = 0;
+        while (sum_request == 0) {
+           for (int i = 0; i < NUM_RESOURCES; i++) {
+               if (need[customer_id][i] == 0) { 
+                    continue;
+                }
+                request[i] = rand() % need[customer_id][i];
+                sum_request += request[i];
+            } 
+        }
+        
+        bool request_outcome = request_res(customer_id, request);
+
+        if (sum_need(customer_id) > 0) {
+            bool release_outcome = release_res(customer_id, allocation[customer_id]);
+            // TODO print
+            break;
+        }
+
+        //TODO print
+    }
+
+    free(customer_id_ptr);
+    pthread_exit(0);
+}
+
+
+
 // // Does the acutal bankers algorithm
-// bool request_res(int n_customer, int request[]){
+// bool request_res(int n_customeyor, int request[]){
 
 //     for (int i = 0; i < NUM_RESOURCES; i++){
 //         printf("%d", request[i]);
@@ -76,3 +110,38 @@ void fill_matrix(){
 // bool release_res(int n_customer, int release[]){
 
 // }
+
+
+int main(int argc, char *argv[]){
+
+    pthread_t threads[NUM_CUSTOMERS];
+
+    if (argc != NUM_RESOURCES + 1) {
+        fprintf(stderr, "Invalid input. Needs the available resources inputted");
+        exit(1);
+    }
+    
+    
+    for (int i = 1; i < argc; i++){
+        available[i-1] = atoi(argv[i]);
+    }
+    
+    fill_matrix();
+
+    for (int i = 0; i < NUM_CUSTOMERS; i++){
+
+        int* customer_id = malloc(sizeof(int));
+        *customer_id = i;
+
+        int rc = pthread_create(&threads[i], NULL, customer, customer_id);
+        if (rc > 0){
+            fprintf(stderr, "ERROR; return code from pthread_create() is %d\n", rc);
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < NUM_CUSTOMERS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+}
